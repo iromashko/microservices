0ab704840bd7c5a90285cb55c4dd470bfb52bf20
iromashko <iromashko@me.com>
2020-09-20 07:49:22 +0300

    get initial props from server

--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -1,3 +1,29 @@
-export default () => {
-  return <h1>Landing Page!!!</h1>;
+import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
+
+  return <h1>Landing Page</h1>;
 };
+
+LandingPage.getInitialProps = async () => {
+  if (typeof window === 'undefined') {
+    const { data } = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+      {
+        headers: {
+          Host: 'ticketing.dev',
+        },
+      }
+    );
+
+    return data;
+  } else {
+    const { data } = await axios.get('/api/users/currentuser');
+
+    return data;
+  }
+  return {};
+};
+
+export default LandingPage;
--- ticketing/notes.md
+++ ticketing/notes.md
@@ -1,1 +1,9 @@
+minikube delete
+
+Install the Ingress Controller: kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/cloud/deploy.yaml
+
+Enable ingress: minikube addons enable ingress
+
 kubectl create secret generic jwt-secret --from-literal=JWT_KEY=asdf
+
+skaffold dev
