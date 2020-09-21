b4264f50ec3f2edbd4b237a2ee6194404500e648
iromashko <iromashko@me.com>
2020-09-21 10:28:34 +0300

    new tickets test

--- ticketing/tickets/src/app.ts
+++ ticketing/tickets/src/app.ts
@@ -2,7 +2,8 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
-import { errorHandler, NotFoundError } from '@irm_tickets/common';
+import { errorHandler, NotFoundError, currentUser } from '@irm_tickets/common';
+import { createTicketRouter } from './routes/new';
 
 const app = express();
 app.set('trust proxy', true);
@@ -13,11 +14,13 @@ app.use(
     secure: process.env.NODE_ENV !== 'test',
   })
 );
+app.use(currentUser);
+app.use(createTicketRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
 });
 
-export { app };
-
 app.use(errorHandler);
+
+export { app };
--- ticketing/tickets/src/test/setup.ts
+++ ticketing/tickets/src/test/setup.ts
@@ -1,12 +1,11 @@
-import request from 'supertest';
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
-import { app } from '../app';
+import jwt from 'jsonwebtoken';
 
 declare global {
   namespace NodeJS {
     interface Global {
-      signin(): Promise<string[]>;
+      signin(): string[];
     }
   }
 }
@@ -38,16 +37,15 @@ afterAll(async () => {
   await mongoose.connection.close();
 });
 
-global.signin = async () => {
-  const email = 'test@test.com';
-  const password = 'password';
+global.signin = () => {
+  const payload = {
+    id: '23423423',
+    email: 'test@test.com',
+  };
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+  const session = { jwt: token };
 
-  const response = await request(app)
-    .post('/api/users/signup')
-    .send({ email, password })
-    .expect(201);
-
-  const cookie = response.get('Set-Cookie');
-
-  return cookie;
+  const sessionJSON = JSON.stringify(session);
+  const base64 = Buffer.from(sessionJSON).toString('base64');
+  return [`express:sess=${base64}`];
 };
