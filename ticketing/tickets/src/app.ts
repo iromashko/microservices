248601b087583168bd6e0ce34c4a71c815360db3
iromashko <iromashko@me.com>
2020-09-21 07:04:50 +0300

    add: tickets service

---
+++ ticketing/tickets/src/app.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+import { errorHandler, NotFoundError } from '@irm_tickets/common';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
+  })
+);
+
+app.all('*', async (req, res) => {
+  throw new NotFoundError();
+});
+
+export { app };
+
+app.use(errorHandler);
