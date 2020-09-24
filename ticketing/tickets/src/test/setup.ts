cd2991cc7476a5f1862fa746c5c1488bc1f00a6e
iromashko <iromashko@me.com>
2020-09-24 04:23:31 +0300

    mock nats wrapper

--- ticketing/tickets/src/test/setup.ts
+++ ticketing/tickets/src/test/setup.ts
@@ -10,6 +10,8 @@ declare global {
   }
 }
 
+jest.mock('../nats-wrapper.ts');
+
 let mongo: any;
 beforeAll(async () => {
   process.env.JWT_KEY = 'asdfasdf';
@@ -26,6 +28,8 @@ beforeAll(async () => {
 
 beforeEach(async () => {
   const collections = await mongoose.connection.db.collections();
+
+  jest.clearAllMocks();
 
   for (let collection of collections) {
     await collection.deleteMany({});
