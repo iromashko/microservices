5f1fa489c95ff0e05bfc3549262a0e8e30c897d9
iromashko <iromashko@me.com>
2020-09-26 06:03:41 +0300

    add: ticket versioning

--- ticketing/orders/src/events/listeners/ticket-updated-listener.ts
+++ ticketing/orders/src/events/listeners/ticket-updated-listener.ts
@@ -7,7 +7,10 @@ export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
   readonly subject = Subjects.TicketUpdated;
   queueGroupName = queueGroupName;
   async onMessage(data: TicketUpdatedEvent['data'], msg: Message) {
-    const ticket = await Ticket.findById(data.id);
+    const ticket = await Ticket.findOne({
+      _id: data.id,
+      version: data.version - 1,
+    });
     if (!ticket) {
       throw new Error('Ticket not found');
     }
