7b6d277eb2d68a46c9ddb8dc86690401b72389c6
iromashko <iromashko@me.com>
2020-09-27 13:00:13 +0300

    add: order created listener

--- ticketing/expiration/src/events/listeners/order-created-listener.ts
+++ ticketing/expiration/src/events/listeners/order-created-listener.ts
@@ -2,12 +2,16 @@ import { Subjects } from '@irm_tickets/common';
 import { Listener } from '@irm_tickets/common/build/events/base-listener';
 import { OrderCreatedEvent } from '@irm_tickets/common/build/events/order-created-event';
 import { Message } from 'node-nats-streaming';
+import { expirationQueue } from '../../queues/expiration-queue';
 import { queueGroupName } from './queue-group-name';
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   readonly subject = Subjects.OrderCreated;
   queueGroupName = queueGroupName;
-  onMessage(data: OrderCreatedEvent['data'], msg: Message) {
-    //
+  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+    await expirationQueue.add({
+      orderId: data.id,
+    });
+    msg.ack();
   }
 }
