0d1760058c997e30e2ce03b9f7d8496ef18173fa
iromashko <iromashko@me.com>
2020-09-29 10:50:37 +0300

    feat: stripe test payment

--- ticketing/notes.md
+++ ticketing/notes.md
@@ -6,6 +6,8 @@ Enable ingress: minikube addons enable ingress
 
 kubectl create secret generic jwt-secret --from-literal=JWT_KEY=asdf
 
+kubectl create secret generic stripe-secret --from-literal=STRIPE_KEY=....
+
 skaffold dev
 
 ---
--- ticketing/payments/src/routes/new.ts
+++ ticketing/payments/src/routes/new.ts
@@ -9,6 +9,7 @@ import {
   OrderStatus,
 } from '@irm_tickets/common';
 import { Order } from '../models/order';
+import { stripe } from '../stripe';
 
 const router = express.Router();
 
@@ -29,6 +30,13 @@ router.post(
     if (order.status === OrderStatus.Cancelled) {
       throw new BadRequestError('Cannot pay for an cancelled order');
     }
+
+    await stripe.charges.create({
+      currency: 'usd',
+      amount: order.price * 100,
+      source: token,
+    });
+
     res.send({ success: true });
   }
 );
